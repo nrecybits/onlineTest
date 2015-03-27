1ea01ea04e7b9202a42c39cbb9dd0de8d4474006
nre <test_nre@cybits.de>
2015-03-27 16:46:01 +0100

    neu datei

---
+++ test_regex.php
@@ -0,0 +1,9 @@
+<?php
+
+//$test="#155#14";//1000#5
+$test="NLD52180471";//1000
+
+$str= preg_replace('/^([x|0|(nld)]){1,}|\s+/i','*',$test);
+ echo "<br>Test : ".  $test ;        
+ echo "<br>STRG : ".  $str ;
+?>
